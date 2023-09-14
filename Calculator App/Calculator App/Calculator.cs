658fe15adb0f983d1d0be0af87c4757871e6269c
roy-eugene049 <roy.eugenes000@gmail.com>
2023-09-14 12:00:26 +0300

    Calculator Logic

---
+++ Calculator App/Calculator App/Calculator.cs
@@ -0,0 +1,27 @@
+﻿public class Calculator
+{
+    public double Add(double num1, double num2)
+    { 
+        return num1 + num2; 
+    }
+
+    public double Substract(double num1, double num2) 
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply(double num1, double num2) 
+    {
+        return num1 * num2;
+    }
+
+    public double Divide(double num1, double num2) 
+    {
+        if (num2 == 0) 
+        {
+            Console.WriteLine("Error: Division by zero!");
+            return 0;
+        }
+        return num1 / num2;
+    }
+}
