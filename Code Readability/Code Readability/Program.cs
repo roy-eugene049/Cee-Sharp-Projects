d155cfc8860e69ed17fc42b443431752a5714f35
roy-eugene049 <roy.eugenes@gmail.com>
2023-09-22 16:23:23 +0300

    Initial Commit

---
+++ Code Readability/Code Readability/Program.cs
@@ -0,0 +1,29 @@
+﻿/* This code reverses a message, counts the number of times a
+   particular character appears, then prints the results 
+   to the console window.
+*/
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+
+char[] message = originalMessage.ToCharArray();
+
+Array.Reverse(message);
+
+int letterCount = 0;
+
+// count the o's
+foreach (char letter in message)
+{
+    if (letter == '0')
+    {
+        letterCount++;
+    }
+}
+
+// convert it back to a string
+string newMessage = new String(message);
+
+// print it out
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
