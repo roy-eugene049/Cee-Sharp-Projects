0eb6f58dee60ca5a9477606105826fe2c1529925
roy-eugene049 <roy.eugenes@gmail.com>
2023-09-22 14:26:45 +0300

    Initial Commit

---
+++ Nested Iteration/Nested Iteration/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] fraudData = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+string letter = "B";
+foreach (string item in fraudData)
+{
+    if (item.StartsWith(letter))
+    {
+        Console.WriteLine(item);
+    }
+}
