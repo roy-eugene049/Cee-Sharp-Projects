acdc6b24b18a2e55d276ba42d99d41bf08c5f45b
roy-eugene049 <roy.eugenes000@gmail.com>
2023-09-26 16:51:12 +0300

    Initial Commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the start of the range: ");
+        int start = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the end of the range: ");
+        int end = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            } else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            } else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            } else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
