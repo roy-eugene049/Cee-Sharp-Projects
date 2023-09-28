8b3f4a7c2356544db2a60d2f29fc58e6474e696a
roy-eugene049 <roy.eugenes000@gmail.com>
2023-09-28 10:38:05 +0300

    Refactor Code Functionality

--- Guess The Number/Guess The Number/Program.cs
+++ Guess The Number/Guess The Number/Program.cs
@@ -2,29 +2,31 @@
 
 class Program
 {
+    public static string userName = Console.ReadLine().ToUpper();
     static void Main()
     {
-        //User welcome message
-        Console.WriteLine("Enter your Name: ");
-        string userName = Console.ReadLine().ToUpper();
+        // User Welcome Message
+        Console.WriteLine("Enter your name: ");
+
         Console.WriteLine($"\nWelcome to the Guess The Number Game {userName}!");
+        Console.WriteLine("........................................ \n");
 
         bool playAgain = true;
 
         while (playAgain)
         {
-            //get the minimum and maximum values for the range
-            Console.WriteLine("........................................ \n");
-            int minimum = GetUserInput("Enter the minimum value: ");
+
+            // Get the minimum and maximum values for the input range
+            int minimum = GetUserInput("Enter the minimum value:  ");
             int maximum = GetUserInput("Enter the maximum value: ");
 
-            //generate a random number within the specified range
+            // Generate a random number within the specified range
             Random random = new Random();
-            int secretNumber = random.Next(minimum, maximum + 1);
+            int SecretNumber = random.Next(minimum, maximum + 1);
             int numberOfGuesses = 0;
 
             Console.WriteLine("........................................ \n");
-            Console.WriteLine($"Guess a number between {minimum} and {maximum}.");
+            Console.WriteLine($"Guess a number between {minimum} and {maximum}");
 
 
             while (true)
@@ -32,27 +34,27 @@ class Program
                 int userGuess = GetUserInput("Enter your guess: ");
                 numberOfGuesses++;
 
-                if (userGuess == secretNumber)
+                if (userGuess == SecretNumber)
                 {
                     Console.WriteLine("........................................ \n");
                     Console.WriteLine($"Congratulations {userName}! You guessed the correct number in {numberOfGuesses} guesses! \n");
                     break;
                 }
-                else if (userGuess < secretNumber)
+                else if (userGuess < SecretNumber)
                 {
-                    Console.WriteLine("Try a higher number.");
+                    Console.WriteLine("Try a higher value You got this!");
                 }
                 else
                 {
-                    Console.WriteLine("Try a lower number");
+                    Console.WriteLine("Try a lower value");
                 }
             }
-            playAgain = GetYesOrNoInput("Do you want to play again? (yes/no)");
+            playAgain = GetYesOrNoInput("Do you want to play again? (yes / no)");
         }
-        Console.WriteLine($"Thanks for playing {userName}!");
+        Console.WriteLine("Thanks for playing!");
     }
 
-    static int GetUserInput (string message)
+    static int GetUserInput(string message)
     {
         int value;
         bool validInput;
@@ -64,29 +66,31 @@ class Program
 
             if (!validInput)
             {
-                Console.WriteLine("Invalid input. Please enter a valid number");
+                Console.WriteLine($"Invalid input {userName}. Please enter a valid number");
             }
         } while (!validInput);
 
         return value;
     }
 
-    static bool GetYesOrNoInput (string message)
+    static bool GetYesOrNoInput(string message)
     {
         while (true)
         {
             Console.Write(message);
             string response = Console.ReadLine().ToLower();
 
-            if ( response == "yes" )
+            if (response == "yes")
             {
                 return true;
-            } else if ( response == "no" )
+            }
+            else if (response == "no")
             {
                 return false;
-            } else
+            }
+            else
             {
-                Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+                Console.WriteLine($"Invalid input {userName}. Please enter 'Yes' or 'No'");
             }
         }
     }
