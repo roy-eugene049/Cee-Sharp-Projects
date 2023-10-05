9fce8f963bb1f1f03cbf18d2cb9fbff87dc64031
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-05 17:18:52 +0300

    Initial Commit

---
+++ Factorial Calculator/Factorial Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number to calculate its factorial");
+        int number = int.Parse(Console.ReadLine());
+
+        if ( number < 0 )
+        {
+            Console.WriteLine("Factorial is undefined for negative number.");
+        } else
+        {
+            long factorial = CalculateFactorial(number);
+            Console.WriteLine($"Factorial of {number} is {factorial}");
+        }
+    }
+
+
+    static long CalculateFactorial(int n)
+    {
+        if ( n == 0 )
+        {
+            return 1;
+        } else
+        {
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
