07fccecb7fc6c8ae8d589d3f42ff3c4b6d07b575
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-05 17:20:52 +0300

    Code refactored to prevent long data type limitations

--- Factorial Calculator/Factorial Calculator/Program.cs
+++ Factorial Calculator/Factorial Calculator/Program.cs
@@ -1,31 +1,43 @@
 ﻿using System;
+using System.Numerics;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter a number to calculate its factorial");
+        Console.Write("Enter a number to calculate its factorial: ");
         int number = int.Parse(Console.ReadLine());
 
-        if ( number < 0 )
+        if (number < 0)
         {
-            Console.WriteLine("Factorial is undefined for negative number.");
-        } else
+            Console.WriteLine("Factorial is undefined for negative numbers.");
+        }
+        else
         {
-            long factorial = CalculateFactorial(number);
+            BigInteger factorial = CalculateFactorial(number);
             Console.WriteLine($"Factorial of {number} is {factorial}");
         }
     }
 
-
-    static long CalculateFactorial(int n)
+    static BigInteger CalculateFactorial(int n)
     {
-        if ( n == 0 )
+        if (n < 0)
+        {
+            throw new ArgumentException("Factorial is undefined for negative numbers.");
+        }
+
+        if (n == 0 || n == 1)
         {
             return 1;
-        } else
+        }
+        else
         {
-            return n * CalculateFactorial(n - 1);
+            BigInteger result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
