2e736963fb1083a01b2790ce0adcd33bcbfb5c41
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-09 10:27:51 +0300

    4th Iteration: Adding Formatting and Image Scraping functionality

--- Web Scrape/Web Scrape/Program.cs
+++ Web Scrape/Web Scrape/Program.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using System;
+using System.IO;
 using System.Net;
 
 class Program
@@ -7,21 +8,31 @@ class Program
     static void Main()
     {
         string url = "https://github.com/roy-eugene049"; // Replace with the URL of the website you want to scrape
-        string pageText = WebScrape(url);
+        string pageText;
+        string profileImageFileName;
 
-        if (!string.IsNullOrEmpty(pageText))
+        if (WebScrape(url, out pageText, out profileImageFileName))
         {
-            Console.WriteLine("Page Text:");
+            Console.WriteLine("Formatted Page Text:");
             Console.WriteLine(pageText);
+
+            if (!string.IsNullOrEmpty(profileImageFileName))
+            {
+                Console.WriteLine("Profile Image:");
+                Console.WriteLine(profileImageFileName);
+            }
         }
         else
         {
-            Console.WriteLine("No text was found on the page.");
+            Console.WriteLine("No data was found on the page.");
         }
     }
 
-    static string WebScrape(string url)
+    static bool WebScrape(string url, out string pageText, out string profileImageFileName)
     {
+        pageText = string.Empty;
+        profileImageFileName = string.Empty;
+
         try
         {
             // Create an HtmlWeb object
@@ -31,14 +42,32 @@ class Program
             var doc = web.Load(url);
 
             // Extract all text content from the page
-            string pageText = WebUtility.HtmlDecode(doc.DocumentNode.InnerText.Trim());
+            pageText = WebUtility.HtmlDecode(doc.DocumentNode.InnerText.Trim());
 
-            return pageText;
+            // Extract and store profile image, if available
+            var imgNode = doc.DocumentNode.SelectSingleNode("//img[@class='profile-image']");
+            if (imgNode != null)
+            {
+                string imgUrl = imgNode.GetAttributeValue("src", string.Empty);
+                if (!string.IsNullOrEmpty(imgUrl))
+                {
+                    // Download and save the profile image
+                    using (var client = new WebClient())
+                    {
+                        profileImageFileName = "profile_image.jpg"; // You can specify a different file name
+                        client.DownloadFile(imgUrl, profileImageFileName);
+                    }
+                }
+            }
+
+            // Format the extracted text content as needed here
+
+            return true;
         }
         catch (Exception e)
         {
             Console.WriteLine($"An error occurred: {e.Message}");
-            return null;
+            return false;
         }
     }
 }
