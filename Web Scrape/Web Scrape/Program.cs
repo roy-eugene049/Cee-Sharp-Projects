130678fd8f789584373e37edb19ceaa2a90dc536
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-09 10:03:19 +0300

    Second Iteration

--- Web Scrape/Web Scrape/Program.cs
+++ Web Scrape/Web Scrape/Program.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.Net;
 
 class Program
 {
     static void Main()
     {
-        string url = "https://dictionary.cambridge.org/dictionary/english/article"; //Replace with actual url
+        string url = "https://github.com/roy-eugene049"; // Replace with the URL of the website you want to scrape
         List<string> articleTitles = WebScrape(url);
 
-        if (articleTitles.Count > 0 )
+        if (articleTitles.Count > 0)
         {
-            Console.WriteLine("Article Titles: ");
-            foreach ( string title in articleTitles )
+            Console.WriteLine("Article Titles:");
+            foreach (string title in articleTitles)
             {
                 Console.WriteLine(title);
             }
-        } else
+        }
+        else
         {
             Console.WriteLine("No article titles were found.");
         }
@@ -28,26 +30,27 @@ class Program
 
         try
         {
-            //Initialize Html Web and HtmlDocument objects 
+            // Create an HtmlWeb object
             var web = new HtmlWeb();
+
+            // Download the webpage content
             var doc = web.Load(url);
 
-            //Use Xpath to select elements with specific attributes
-            var nodes = doc.DocumentNode.SelectNodes("//a[class='article-title']");
+            // Use XPath to select elements with specific attributes
+            var nodes = doc.DocumentNode.SelectNodes("//h2[@class='article-title']");
 
             if (nodes != null)
             {
-                foreach ( var node in nodes )
+                foreach (var node in nodes)
                 {
-                    //Extract and store the text content of selected elements
-                    articleTitles.Add(node.InnerText.Trim());
+                    // Extract and store the text content of selected elements
+                    articleTitles.Add(WebUtility.HtmlDecode(node.InnerText.Trim()));
                 }
             }
         }
-
         catch (Exception e)
         {
-            Console.WriteLine($"An error occured: {e.Message}");
+            Console.WriteLine($"An error occurred: {e.Message}");
         }
 
         return articleTitles;
