db4fdd9846b7563dda6b5c70ba7f9f370e56c393
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-09 14:29:44 +0300

    Initial Commit

---
+++ Variable Scope/Variable Scope/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //normal syntax
+        //bool flag = true;
+        //if (flag)
+        //{
+        //    Console.WriteLine(flag);
+        //}
+
+        ////single line form if statements
+        //bool flag = true;
+        //if (flag) Console.WriteLine(flag);
+
+
+        //larger id elseif else constructs
+        string name = "steve";
+        if (name == "bob") Console.WriteLine("Found Bob");
+        else if (name == "steve") Console.WriteLine("Found Steve");
+        else Console.WriteLine("Found Chuck");
+    }
+}
