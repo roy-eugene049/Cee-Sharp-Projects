b87e077b2fbc161650bbee7f14715b5751084676
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-09 08:59:16 +0300

    Initial Commit

---
+++ Bool Expressions Challenge/Bool Expressions Challenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string permission = "Admin|Manager";
+        int level = 55;
+
+        switch (permission)
+        {
+            case var _ when permission.Contains("Admin") && level > 55:
+                Console.WriteLine("Welcome, Super Admin user.");
+                break;
+
+            case var _ when permission.Contains("Admin") && level <= 55:
+                Console.WriteLine("Welcome, Admin user.");
+                break;
+
+            case var _ when permission.Contains("Manager") && level >= 20:
+                Console.WriteLine("Contact an Admin for access.");
+                break;
+
+            case var _ when permission.Contains("Manager") && level < 20:
+                Console.WriteLine("You do not have sufficient privileges.");
+                break;
+
+            default:
+                Console.WriteLine("You do not have sufficient privileges.");
+                break;
+        }
+    }
+}
