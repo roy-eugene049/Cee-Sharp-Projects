29b9be17efc9da360c09329b454a42300fe497cd
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-14 15:18:07 +0300

    Variable Declaration

---
+++ Contoso Pets App/Contoso Pets App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // the ourAnimals array will store the following: 
+        string animalSpecies = "";
+        string animalID = "";
+        string animalAge = "";
+        string animalPhysicalDescription = "";
+        string animalPersonalityDescription = "";
+        string animalNickname = "";
+
+        // variables that support data entry
+        int maxPets = 8;
+        string? readResult;
+        string menuSelection = "";
+
+        // array used to store runtime data, there is no persisted data
+        string[,] ourAnimals = new string[maxPets, 6];
+    }
+}
