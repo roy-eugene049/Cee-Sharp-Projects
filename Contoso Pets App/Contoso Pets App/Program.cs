38bc485384ff6246f02a7543b9384011ba6dc494
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-16 16:52:45 +0300

    Display message output when petCount is < maxPets

--- Contoso Pets App/Contoso Pets App/Program.cs
+++ Contoso Pets App/Contoso Pets App/Program.cs
@@ -137,6 +137,12 @@ if (readResult != null)
                     petCount += 1;
                 }
             }
+
+            if (petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
+            }
+
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
