6cf764194ebb14b027b50e2d36310b2c04641dea
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-16 16:49:48 +0300

    Count the number of pets in the ourAnimals array

--- Contoso Pets App/Contoso Pets App/Program.cs
+++ Contoso Pets App/Contoso Pets App/Program.cs
@@ -128,7 +128,15 @@ if (readResult != null)
 
         case "2":
             // Add a new animal friend to the ourAnimals array
-            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            string anotherPet = "y";
+            int petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+            }
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
