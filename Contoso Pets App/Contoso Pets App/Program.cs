a12ff8f3c8c290bf2b0d8a0111e81c9e057a1bb6
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-16 17:11:13 +0300

    Loop to read and validate the pet species

--- Contoso Pets App/Contoso Pets App/Program.cs
+++ Contoso Pets App/Contoso Pets App/Program.cs
@@ -145,6 +145,28 @@ if (readResult != null)
 
             while (anotherPet == "y" && petCount < maxPets)
             {
+                bool validEntry = false;
+                // get species (cat or dog) - string animalSpecies is a required field 
+                do
+                {
+                    Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+                    readResult = Console.ReadLine();
+
+                    if (readResult != null)
+                    {
+                        animalSpecies = readResult.ToLower();
+                        if (animalSpecies != "dog" && animalSpecies != "cat")
+                        {
+                            validEntry = false;
+                        }
+                        else
+                        {
+                            validEntry = true;
+                        }
+                    }
+
+                } while (validEntry == false);
+
                 // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
                 petCount = petCount + 1;
                 // check maxPet limit
