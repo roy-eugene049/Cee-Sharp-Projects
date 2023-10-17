a1a8650695a8433741b014078472f8fd7cf57a1c
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-17 15:23:20 +0300

    Approximating PI

---
+++ Monte Carlo Algorithms/approximatingPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MonteCarloPi
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int insideCircle = 0;
+        int totalPoints = 1000000; // Number of random points
+
+        for (int i = 0; i < totalPoints; i++)
+        {
+            double x = random.NextDouble();
+            double y = random.NextDouble();
+            double distance = x * x + y * y;
+
+            if (distance <= 1)
+                insideCircle++;
+        }
+
+        double estimatedPi = 4.0 * insideCircle / totalPoints;
+        Console.WriteLine($"Estimated π with {totalPoints} samples: {estimatedPi}");
+    }
+}
