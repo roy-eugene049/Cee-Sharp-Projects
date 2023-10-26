e9e82105c996f5e7c5c892ba59f3237569d4e80b
roy-eugene049 <roy.eugenes000@gmail.com>
2023-10-26 15:56:20 +0300

    Initial Commit

---
+++ Class and Object Definition/Class and Object Definition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ClassDemo
+{
+    class Sum
+    {
+        private int x;
+        private int y;
+        public void setVal(int val1, int val2)
+        {
+            x = val1;
+            y = val2;
+        }
+        public int getSum()
+        {
+            return x + y;
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Sum s = new Sum();
+            s.setVal(3, 14);
+            Console.WriteLine("Sum of 3 and 14 is: {0}", s.getSum());
+        }
+    }
+}
