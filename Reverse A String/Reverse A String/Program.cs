1369019f5d88767606d131c4337637ba3708e3b6
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-07 16:29:04 +0300

    Initial Commit

---
+++ Reverse A String/Reverse A String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = Console.ReadLine();
+        ReverseString(inputString);
+    }
+
+    internal static void ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+        {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+        }
+        string reversedString = new string(charArray);
+        Console.WriteLine(reversedString);
+    }
+}
