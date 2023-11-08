e8e14dc6f7eeef980e726df33ec8544f000b5980
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-08 18:15:22 +0300

    Intial Commit

---
+++ Remove Duplicate Chars from a String/Remove Duplicate Chars from a String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    internal static void removeduplicate(string str)
+    {
+        string result = string.Empty;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (!result.Contains(str[i]))
+            {
+                result += str[i];
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
