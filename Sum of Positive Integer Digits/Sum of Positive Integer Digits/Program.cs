b0550e7f55f957f61c89f647f4f0bd2405f14824
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-10 11:58:11 +0300

    Add SumOfDigits method

--- Sum of Positive Integer Digits/Sum of Positive Integer Digits/Program.cs
+++ Sum of Positive Integer Digits/Sum of Positive Integer Digits/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+    }
 
+    internal static void SumOfDigits(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        Console.WriteLine(sum);
     }
 }
