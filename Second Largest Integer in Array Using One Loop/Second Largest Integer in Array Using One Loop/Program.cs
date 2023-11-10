78873f5ebd49aeac310ae4da6f80719bef4d1ae5
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-10 12:10:01 +0300

    Adding FindSecondLargeInArray Method

--- Second Largest Integer in Array Using One Loop/Second Largest Integer in Array Using One Loop/Program.cs
+++ Second Largest Integer in Array Using One Loop/Second Largest Integer in Array Using One Loop/Program.cs
@@ -6,4 +6,23 @@ class Program
     {
 
     }
+
+    internal static void FindSecondLargeInArray(int[] arr)
+    {
+        int max1 = int.MinValue;
+        int max2 = int.MinValue;
+        foreach (int i in arr)
+        {
+            if (i > max1)
+            {
+                max2 = max1;
+                max1 = i;
+            }
+            else if (i >= max2 && i != max1)
+            {
+                max2 = i;
+            }
+        }
+        Console.WriteLine(max2); ;
+    }
 }
