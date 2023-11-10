d5731729cb0158ff4d5218502426964f69f0f41d
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-10 11:44:01 +0300

    Add FindPrime method

--- Prime Num Check/Prime Num Check/Program.cs
+++ Prime Num Check/Prime Num Check/Program.cs
@@ -7,4 +7,19 @@ class Program
 
     }
 
+    internal static bool FindPrime(int number)
+    {
+        if (number == 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        var squareRoot = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= squareRoot; i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
 }
