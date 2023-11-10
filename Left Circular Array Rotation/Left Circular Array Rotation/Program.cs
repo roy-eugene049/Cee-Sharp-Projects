d2325a5b471302ea87766ce13854a352c910c65f
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-10 11:29:21 +0300

    Initial Commit

---
+++ Left Circular Array Rotation/Left Circular Array Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    internal static void RotateLeft(int[] array)
+    {
+        int size = array.Length;
+        int temp;
+        for (int j = size - 1; j > 0; j--)
+        {
+            temp = array[size - 1];
+            array[array.Length - 1] = array[j - 1];
+            array[j - 1] = temp;
+        }
+
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
