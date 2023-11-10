c541d773bbd76e23176685eef39a94c5f69e2bfb
roy-eugene049 <roy.eugenes000@gmail.com>
2023-11-10 11:12:13 +0300

    Get User Input

--- Find Substring/Find Substring/Program.cs
+++ Find Substring/Find Substring/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string");
+        string userString = Console.ReadLine();
+        findallsubstring(userString);
+    }
+
+    internal static void findallsubstring(string str)
+    {
+        for (int i = 0; i < str.Length; ++i)
+        {
+            StringBuilder subString = new StringBuilder(str.Length - i);
+            for (int j = i; j < str.Length; ++j)
+            {
+                subString.Append(str[j]);
+                Console.Write(subString + " ");
+            }
+        }
+    }
+}
